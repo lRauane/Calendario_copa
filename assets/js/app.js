300ebdd158c56f50f65a3a88533d1b7bcee5cf65
Rauane Lima <rauanelima7@gmail.com>
2022-11-04 09:58:40 -0300

    _Finalizando projeto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 function createGame(player1, hour, player2) {
   return `
-  <li><img src="./assets/components/icon-${player1}.svg" alt="Flag of ${player1}">
+  <li><img src="./assets/components/flags/icon-${player1}.svg" alt="Flag of ${player1}">
         <strong>${hour}</strong>
-        <img src="./assets/components/icon-${player2}.svg" alt="Flag of ${player2}">
-      </li>
+        <img src="./assets/components/flags/icon-${player2}.svg" alt="Flag of ${player2}">
+  </li>
+      
   `;
 }
 let delay = -0.3;
@@ -20,6 +21,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "Quinta", createGame("brazil", "16:00", "cameroon")) +
+  createCard("21/11", "Quinta", createGame("brazil", "16:00", "cameroon"))
+   +
   createCard("28/11", "Segunda", createGame("argentina", "18:00", "hungary")) +
   createCard("02/11", "sexta", createGame("argentina", "18:00", "hungary"));
